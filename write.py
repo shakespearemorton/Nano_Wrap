c3549e19ef397a3b988a6a87898de3000f4a5551
shakespearemorton <45985979+shakespearemorton@users.noreply.github.com>
2024-03-26 17:11:00 +0000

    Add files via upload

---
+++ write.py
@@ -0,0 +1,119 @@
+import numpy as np
+import datetime
+import random
+
+def writeMembrane(pos,receptor_f,boxx,boxy,boxz):
+    with open( 'membrane.txt', 'w' ) as g:
+        nParticles = len(pos)
+        bonds = nParticles*(2/3)
+        angles = nParticles/3
+        g.write("#NP 10*10*20\n#second line will be skipped\n\n" )
+        g.write( "{0:.0f}     atoms\n".format( nParticles) )
+        g.write( "{0:.0f}     bonds\n".format(bonds ))
+        g.write("{0:.0f}    angles\n".format( angles))
+        g.write("0  dihedrals\n" )
+        g.write("0  impropers\n\n" )
+        g.write("7 atom types\n1 bond types\n1 angle types\n0  dihedral types\n0  improper types\n\n")
+        g.write("0 {0:.2f} xlo xhi\n0 {1:.2f} ylo yhi\n-{2:.2f} {2:.2f} zlo zhi".format(boxx,boxy,boxz/2))
+        g.write("\n\nMasses\n\n1 1.0\n2 1.0\n3 1.0\n4 1.0\n5 1.0\n6 1.0\n7 1.0\n")
+        g.write("\nAtoms\n\n")
+        t=0
+        receps = []
+        ops = list(range(len(pos)))
+        while len(receps) < int(len(pos)*receptor_f):
+            rch = random.choice(range(len(ops)))
+            receps.append(ops[rch])
+            ops.pop(rch)
+        mol = 1
+        while t < nParticles:
+            if int(t/3) in receps:
+                g.write( "{0:.0f} {4:.0f} 3 0.0 {1:.3f} {2:.3f} {3:.3f} \n".format( t+1, pos[ t, 0 ], pos[ t, 1 ], pos[ t, 2 ],mol ) )
+                t+=1
+            else:
+                g.write( "{0:.0f} {4:.0f}  1 0.0 {1:.3f} {2:.3f} {3:.3f} \n".format( t+1, pos[ t, 0 ], pos[ t, 1 ], pos[ t, 2 ],mol ) )
+                t+=1
+            g.write( "{0:.0f} {4:.0f} 2 0.0 {1:.3f} {2:.3f} {3:.3f} \n".format( t+1, pos[ t, 0 ], pos[ t, 1 ], pos[ t, 2 ],mol ) )
+            t+=1
+            g.write( "{0:.0f} {4:.0f} 2 0.0 {1:.3f} {2:.3f} {3:.3f} \n".format( t+1, pos[ t, 0 ], pos[ t, 1 ], pos[ t, 2 ],mol ) )
+            t+=1
+            mol+=1
+        g.write ("\nBonds\n\n")
+        t = 0
+        n=0
+        while n < bonds:
+            g.write( "{0:.0f} 1 {1:.0f} {2:.0f} \n".format( n+1, int(t+1), int(t+2) ) )
+            n+=1
+            t+=1
+            g.write( "{0:.0f} 1 {1:.0f} {2:.0f} \n".format( n+1, int(t+1), int(t+2) ) )
+            t+=2
+            n+=1
+        g.write ("\nAngles\n\n")
+        t = 0
+        n=0
+        while n < angles:
+            g.write( "{0:.0f} 1 {1:.0f} {2:.0f} {3:.0f} \n".format( n+1, int(t+1), int(t+2), int(t+3) ) )
+            n+=1
+            t+=3
+
+        g.close()
+    return
+
+def writeParticle(core,tips,binding_f,binding_style,memb,filler=[]):
+
+    with open( 'particle.txt', 'w' ) as g:
+        if len(tips) == 0:
+            star = core
+        else:
+            star = np.concatenate((core,tips))
+        box_max = np.max(star[:,2],axis=0)+10
+        if binding_style == 'Even':
+            receps = []
+            ops = list(range(len(star)))
+            while len(receps) < int(len(star)*binding_f):
+                rch = random.choice(range(len(ops)))
+                receps.append(ops[rch])
+                ops.pop(rch)
+        elif binding_style == 'Num':
+            receps = []
+            ops = list(range(len(star)))
+            while len(receps) < int(binding_f):
+                rch = random.choice(range(len(ops)))
+                receps.append(ops[rch])
+                ops.pop(rch)
+        nParticles = len(star)
+        g.write("#NP 10*10*20\n#second line will be skipped\n\n" )
+        g.write( "{0:.0f}     atoms\n".format( nParticles+len(filler)) )
+        g.write( "0     bonds\n")
+        g.write("0    angles\n")
+        g.write("0  dihedrals\n" )
+        g.write("0  impropers\n\n" )
+        g.write("7 atom types\n1 bond types\n1 angle types\n0  dihedral types\n0  improper types\n\n")
+        g.write("{0:.2f} {1:.2f} xlo xhi\n{2:.2f} {3:.2f} ylo yhi\n{4:.2f} {5:.2f} zlo zhi".format(memb[0,0],memb[0,1],memb[1,0],memb[1,1],-3*box_max,3*box_max))
+        g.write("\nAtoms\n\n")
+        t=0
+        for i in core:
+            if t in receps:
+                g.write( "{0:.0f} 1 4 0.0 {1:.3f} {2:.3f} {3:.3f} \n".format( t+1, i[ 0 ], i[ 1 ], i[ 2 ] ) )
+            else:
+                g.write( "{0:.0f} 1 5 0.0 {1:.3f} {2:.3f} {3:.3f} \n".format( t+1, i[ 0 ], i[ 1 ], i[ 2 ] ) )
+            t+=1
+        
+        for i in tips:
+            if t in receps:
+                g.write( "{0:.0f} 1 6 0.0 {1:.3f} {2:.3f} {3:.3f} \n".format( t+1, i[ 0 ], i[ 1 ], i[ 2 ] ) )
+            else:
+                g.write( "{0:.0f} 1 7 0.0 {1:.3f} {2:.3f} {3:.3f} \n".format( t+1, i[ 0 ], i[ 1 ], i[ 2 ] ) )
+            t+=1
+        g.close()
+    return
+
+def writeVMD(pos,name='VMD'):
+    with open( str(name + '.xyz'), 'w' ) as g:
+        num = len(pos)
+        g.write(repr(num)+'\n\n')
+        t = 0
+        while t < (len(pos)):
+            xyz = np.array((float(pos[t][0]),float(pos[t][1]),float(pos[t][2])))
+            g.write( "C {0:.5f} {1:.5f} {2:.5f} \n".format( xyz[0], xyz[1], xyz[2] ) )
+            t+=1
+    return
